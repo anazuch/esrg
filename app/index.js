44362c04d53c3f3a2d2f6f16a155fbe0c052a608
Ana Carolina Zuchetto <carolzuchetto@gmail.com>
2017-08-07 23:17:05 -0300

    Add new elements
    Added gitignore
    Added doc with knowledge nomenclatures.txt
    Added Electron (Html motor render)

---
+++ app/index.js
@@ -0,0 +1,44 @@
+(function() {
+    const electron = require('electron');
+    const app = electron.app;
+    const BrowserWindow = electron.BrowserWindow;
+    const globalShortcut = electron.globalShortcut;
+    const path = require('path');
+    const url = require('url');
+    let mainWindow;
+
+    function createWindow() {
+        mainWindow = new BrowserWindow({
+            maximizable: true,
+            minimizable: true,
+            closable: true,
+            movable: true,
+            title: "ESRG",
+            show: true,
+            autoHideMenuBar: true,
+            titleBarStyle: 'hidden-inset'
+        })
+
+        mainWindow.loadURL(`file://${__dirname}/source/index.html`);
+        // mainWindow.webContents.openDevTools();
+        mainWindow.on('closed', function() {
+            mainWindow = null;
+        })
+        mainWindow.maximize()
+    }
+
+    app.on('ready', createWindow);
+
+    app.on('window-all-closed', function() {
+        if (process.platform !== 'darwin') {
+            app.quit();
+        }
+    })
+
+    app.on('activate', function() {
+        if (mainWindow === null) {
+            createWindow();
+        }
+    })
+
+})();
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Initializing the ESRG application
+cd app
+npm start
